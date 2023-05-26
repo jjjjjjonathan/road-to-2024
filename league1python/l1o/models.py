8ea208596b770879105c7ec6ab48e4a9278f619c
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-05-26 10:29:43 -0400

    Update models to add divisions, teams, and matches

--- league1python/l1o/models.py
+++ league1python/l1o/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 
-class Teacher(models.Model):
+class Divisions(models.Model):
     name = models.CharField(max_length=80)
-    age = models.IntegerField()
+
+
+class Teams(models.Model):
+    name = models.CharField(max_length=80)
+    points_in_2022 = models.IntegerField()
+    division = models.ForeignKey(Divisions, on_delete=models.CASCADE)
+
+
+class Matches(models.Model):
+    home_team = models.ForeignKey(
+        Teams, on_delete=models.CASCADE, related_name="home_team"
+    )
+    away_team = models.ForeignKey(
+        Teams, on_delete=models.CASCADE, related_name="away_team"
+    )
+    division = models.ForeignKey(Divisions, on_delete=models.CASCADE)
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+    e2e_id = models.IntegerField()
