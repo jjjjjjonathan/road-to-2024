46b1e6089d788772dc0385f70280cb97cc8bc414
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-05-29 23:20:11 -0400

    Write methods for teams to calculate wins, losses, draws, points

--- league1python/l1o/models.py
+++ league1python/l1o/models.py
@@ -4,21 +4,67 @@ from django.db import models
 class Divisions(models.Model):
     name = models.CharField(max_length=80)
 
+    def __str__(self):
+        return self.name
+
 
 class Teams(models.Model):
     name = models.CharField(max_length=80)
     points_in_2022 = models.IntegerField()
-    division = models.ForeignKey(Divisions, on_delete=models.CASCADE)
+    division = models.ForeignKey(
+        Divisions, on_delete=models.CASCADE, related_name="division"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def wins(self):
+        home_wins = self.home_matches.filter(
+            home_score__gt=models.F("away_score")
+        ).count()
+        away_wins = self.away_matches.filter(
+            away_score__gt=models.F("home_score")
+        ).count()
+        return home_wins + away_wins
+
+    def losses(self):
+        home_losses = self.home_matches.filter(
+            home_score__lt=models.F("away_score")
+        ).count()
+        away_losses = self.away_matches.filter(
+            away_score__gt=models.F("home_score")
+        ).count()
+        return home_losses + away_losses
+
+    def draws(self):
+        home_draws = self.home_matches.filter(
+            home_score__exact=models.F("away_score")
+        ).count()
+        away_draws = self.away_matches.filter(
+            away_score__exact=models.F("home_score")
+        ).count()
+        return home_draws + away_draws
+
+    def points_in_2023(self):
+        return (self.wins() * 3) + (self.draws())
+
+    def total_points(self):
+        return self.points_in_2023() + (self.points_in_2022 * 0.75)
 
 
 class Matches(models.Model):
     home_team = models.ForeignKey(
-        Teams, on_delete=models.CASCADE, related_name="home_team"
+        Teams, on_delete=models.CASCADE, related_name="home_matches"
     )
     away_team = models.ForeignKey(
-        Teams, on_delete=models.CASCADE, related_name="away_team"
+        Teams, on_delete=models.CASCADE, related_name="away_matches"
     )
-    division = models.ForeignKey(Divisions, on_delete=models.CASCADE)
+    division = models.ForeignKey(
+        Divisions, on_delete=models.CASCADE, related_name="division"
+    )
     home_score = models.IntegerField()
     away_score = models.IntegerField()
     e2e_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.home_team} vs. {self.away_team}"
--- league1python/league1python/settings.py
+++ league1python/league1python/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "l1o",
+    "l1o.apps.L1OConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
