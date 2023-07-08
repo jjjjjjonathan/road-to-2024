88f0389a90e0663accb3d8e0c0c4c5dc104edf97
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-07-07 22:36:27 -0400

    Add functions to calculate playoff contention

--- l1o/templatetags/l1o_extras.py
+++ l1o/templatetags/l1o_extras.py
@@ -11,5 +11,15 @@ def clinched_relegation(max_possible_points, relegation_threshold):
     return max_possible_points < relegation_threshold
 
 
+def clinched_playoffs(points, playoff_threshold):
+    return points > playoff_threshold
+
+
+def eliminated_from_playoffs(max_possible_points, sixth_place_points):
+    return max_possible_points < sixth_place_points
+
+
 register.filter("clinched_promotion", clinched_promotion)
 register.filter("clinched_relegation", clinched_relegation)
+register.filter("clinched_playoffs", clinched_playoffs)
+register.filter("eliminated_from_playoffs", eliminated_from_playoffs)
